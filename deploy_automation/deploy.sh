d72ce4226f5e088458b3ecbefaa9b05252678f46
Christian Bagdon <cbagdon119@gmail.com>
2019-05-03 01:20:18 -0700

    Initial Commit

---
+++ deploy_automation/deploy.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+#Yeah, I know this could have been a lot better and used an actual deploy system.
+#idc. I want level 7. pls.
+
+apt -y update
+apt -y upgrade
+
+#Create user and add to sudo
+
+adduser cbagdon
+adduser cbagdon sudo
+
+#Copy over dummy netplan YML config and apply the settings
+
+rm -rf /etc/netplan/01-netcfg.yaml
+cp assets/netplan_config/01-netcfg.yaml /etc/netplan/
+
+netplan apply
+
+#Install SSH and configure it properly
+
+apt -y install openssh-server
+
+rm -rf /etc/ssh/sshd_config
+cp assets/sshd/sshd_config /etc/ssh/
+ssh-keygen
+mkdir ~/.ssh
+cat assets/ssh/id_rsa.pub > ~/.ssh/authorized_keys
+
+service sshd restart
+
+#Install and configure Fail2Ban
+apt -y install fail2ban
+
+rm -rf /etc/fail2ban/jail.local
+cp assets/fail2ban/jail.local /etc/fail2ban/
+
+cp assets/fail2ban/nginx-dos.conf /etc/fail2ban/filter.d
+cp assets/fail2ban/portscan.conf /etc/fail2ban/filter.d
+
+service fail2ban restart
+
+#Copy and set up cron scripts for updating packages and detecting crontab changes
+
+apt install mailutils
+
+cp -r assets/scripts /home/cbagdon
+{ crontab -l -u cbagdon; echo '0 4 * * SUN /home/cbagdon/scripts/update_script.sh'; } | crontab -u cbagdon -
+{ crontab -l -u cbagdon; echo '@reboot /home/christian/scripts/update_script.sh'; } | crontab -u cbagdon -
+
+{ crontab -l -u cbagdon; echo '0 0 * * * SUN /home/cbagdon/scripts/check_cron.sh'; } | crontab -u cbagdon -
+
+#Set up nginx and copy website files over
+apt -y install nginx
+
+rm -rf /var/www/html/index.nginx-debian.html
+cp assets/nginx/index.nginx-debian.html
+
+#Set up SSL
+cp assets/ssl/self-signed.conf /etc/nginx/snippets/
+cp assets/ssl/ssl-params.conf /etc/nginx/snippets/
+
+rm -rf /etc/nginx/sites-available/default
+cp assets/ssl/default /etc/nginx/sites-available/
+
+#Set up Firewall; Default DROP connections
+ufw enable
+ufw default deny incoming
+ufw default allow outgoing
+ufw allow 5647
+ufw allow 443
+ufw allow 80
+ufw allow 'Nginx Full'
+ufw reload
+
+#Reboot Nginx server, hopefully we have a live website
+systemctl restart nginx
